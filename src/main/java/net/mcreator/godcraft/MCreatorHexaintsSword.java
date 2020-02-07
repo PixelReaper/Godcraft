246e50462c482ec8d11f455709bbd70071a82fc5
Ragnar <pixelreaper@disroot.org>
2020-02-07 10:13:13 +0100

    Hexaints Sword Added

---
+++ src/main/java/net/mcreator/godcraft/MCreatorHexaintsSword.java
@@ -0,0 +1,77 @@
+package net.mcreator.godcraft;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+
+import java.util.Set;
+import java.util.HashMap;
+
+import com.google.common.collect.Multimap;
+
+@Elementsgodcraft.ModElement.Tag
+public class MCreatorHexaintsSword extends Elementsgodcraft.ModElement {
+	@GameRegistry.ObjectHolder("godcraft:hexaintssword")
+	public static final Item block = null;
+
+	public MCreatorHexaintsSword(Elementsgodcraft instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemSword(EnumHelper.addToolMaterial("HEXAINTSSWORD", 3, 10, 4f, 0f, 2)) {
+			public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot slot) {
+				Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
+				if (slot == EntityEquipmentSlot.MAINHAND) {
+					multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier",
+							(double) this.getAttackDamage(), 0));
+					multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -3,
+							0));
+				}
+				return multimap;
+			}
+
+			public Set<String> getToolClasses(ItemStack stack) {
+				HashMap<String, Integer> ret = new HashMap<String, Integer>();
+				ret.put("sword", 3);
+				return ret.keySet();
+			}
+
+			@Override
+			public boolean hitEntity(ItemStack itemstack, EntityLivingBase entity, EntityLivingBase entity2) {
+				super.hitEntity(itemstack, entity, entity2);
+				int x = (int) entity.posX;
+				int y = (int) entity.posY;
+				int z = (int) entity.posZ;
+				World world = entity.world;
+				{
+					java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+					$_dependencies.put("entity", entity);
+					MCreatorHexaintsSwordAttack.executeProcedure($_dependencies);
+				}
+				return true;
+			}
+		}.setUnlocalizedName("hexaintssword").setRegistryName("hexaintssword").setCreativeTab(CreativeTabs.COMBAT));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("godcraft:hexaintssword", "inventory"));
+	}
+}
